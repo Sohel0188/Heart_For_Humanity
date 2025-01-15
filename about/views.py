6b9713a967eb9cc7421353ab5aced19eca87122c
Sohel0188 <sohelsikder.cse@gmail.com>
2025-01-16 01:30:50 +0600

    Make about Serializer

--- about/views.py
+++ about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from . import serializers
+from . import models
 # Create your views here.
+class AboutViewSet(viewsets.ModelViewSet):
+    queryset = models.About_us.objects.all()
+    serializer_class = serializers.AboutSerializers
