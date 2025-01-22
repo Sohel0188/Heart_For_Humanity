3150e366b792a93cd8dec29705379ff32cabca97
Sohel0188 <sohelsikder.cse@gmail.com>
2025-01-22 23:20:16 +0600

    see single post by slug insted of category id

--- campain/views.py
+++ campain/views.py
@@ -9,16 +9,17 @@ from . import models
 class CampainCategoryViewSet(viewsets.ModelViewSet):
     queryset = models.Campain_Category.objects.all()
     serializer_class = serializers.CampainCategorySerializers
+    lookup_field = 'slug'
     
 class CampainViewSet(viewsets.ModelViewSet):
     queryset = models.Campain.objects.all()
     serializer_class = serializers.CampainSerializers
-    
+    lookup_field = 'campain_slug'
     def get_queryset(self):
         queryset = super().get_queryset()
-        slug = self.request.query_params.get('category')
-        if slug :
-            queryset = queryset.filter(slug=slug)
+        slug = self.request.query_params.get('category')  # Updated parameter name
+        if slug:
+            queryset = queryset.filter(category=slug)
         return queryset
 
         
