75af0146921b6fb32b07e5a9dd4117fd6ab63c26
Sohel0188 <sohelsikder.cse@gmail.com>
2025-01-25 23:23:06 +0600

    update cors header

--- Heart_For_Humanity/settings.py
+++ Heart_For_Humanity/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    
     'about',
     'account',
     'blogs',
     'campain',
     'events',
     'contact',
+    'corsheaders',
     
 ]
 
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware", 
 ]
 
 ROOT_URLCONF = 'Heart_For_Humanity.urls'
